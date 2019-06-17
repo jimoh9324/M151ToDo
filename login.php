de672c3158d55919ec76ab12fd287f54ac5f0d72
Jimoh Meyer <meyeji@sti.local>
2019-06-17 16:08:43 +0200

    Header geändert

--- login.php
+++ login.php
@@ -103,7 +103,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && empty($error)){
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Registrierung</title>
+    <title>Anmelden</title>
 
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
