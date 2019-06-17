c003bbf09053343850cbb5ab9babcc58808d9bb2
Jimoh Meyer <meyeji@sti.local>
2019-06-17 15:43:29 +0200

    Titel + Kurzbeschreibung

--- index.php
+++ index.php
@@ -5,11 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Welcome</title>
-
-        <!-- Bootstrap -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+</head>
+<body>
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 
+    <div class='container'>
+        <h1>ToDo Verwaltung</h1>
+        <p>Willkommen zur ToDo Verwaltung. Um die Applikation nutzen zu können, registrieren Sie sich bitte. Falls sie bereits einen Account haben, melden Sie sich bitte an.</p>
+    </div>
 
 
 
@@ -22,8 +27,5 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-</head>
-<body>
-    
 </body>
 </html>
