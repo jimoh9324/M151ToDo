066c74c4e4439cf65619da34ce4860f2924e7d35
Jimoh Meyer <meyeji@sti.local>
2019-06-20 11:31:23 +0200

    Was es in den Files noch zu erledigen gibt hinzugefügt

--- createtodo.php
+++ createtodo.php
@@ -1,1 +1,9 @@
- 
+ <?php
+    //To Dos for this file:
+        //Durch VALIDIERTE Felder sollen die Spalten der DB befüllt werden
+        //File soll nur zugänglich sein wenn man eingeloggt ist
+        //
+
+
+
+ ?>
